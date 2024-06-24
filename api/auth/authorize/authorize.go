cb3eff7810fcb1fbd8d57d4a0906d34735137f85
Megakuul <megakuul@gmx.ch>
2024-06-24 23:11:34 +0200

    feat: start implementing OAuth2.0 client

---
+++ api/auth/authorize/authorize.go
@@ -0,0 +1,27 @@
+package authorize
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+// HandleAuthorization redirects the user to the authorization endpoint of the Cognito provider.
+// Based on https://docs.aws.amazon.com/cognito/latest/developerguide/authorization-endpoint.html
+func HandleAuthorization(request events.APIGatewayV2HTTPRequest, providerDomain, clientId, redirectUri string) (events.APIGatewayV2HTTPResponse, error) {
+	// Hardcoded default scopes are openid (to enable openid connect) and profile (to acquire basic user information).
+	authScopes := "openid profile"
+
+	// Format is based on the cognito endpoint spec: https://docs.aws.amazon.com/cognito/latest/developerguide/authorization-endpoint.html
+	authUrl := fmt.Sprintf(
+		"%s/oauth2/authorize?response_type=code&client_id=%s&redirect_uri=%s&scope=%s",
+		providerDomain, clientId, redirectUri, authScopes,
+	)
+
+	return events.APIGatewayV2HTTPResponse{
+		StatusCode: 302,
+		Headers: map[string]string{
+			"Location": authUrl,
+		},
+	}, nil
+}
